f6d29b7fc68e7d46d2b7bad4033ef433a82df24c
xuqing <qing.xu2@ofilm.com>
2024-03-20 14:06:33 +0800

    1:loss.py change lossxy losslen lossthe weight from (1, 20, 10) to (1, 15, 10)
    2:hpy bos&obj loss weight from (10,0.05) to (1,0.5)
    
    3:this version can get map95=0.58 at ps2.0 database

--- utils/loss.py
+++ utils/loss.py
@@ -153,7 +153,7 @@ class ComputeLoss:
                     lossxy = self.MSEwh(pbox[:,0:2], tbox[i][:,0:2])
                     losslen = self.MSEwh(pbox[:,2:3], tbox[i][:,2:3])
                     lossthe = self.MSEwh(pbox[:,3:], tbox[i][:,3:])
-                    lbox += lossxy * 1 + losslen * 20 + lossthe * 10
+                    lbox += lossxy * 1 + losslen * 15 + lossthe * 10
                     #lbox += lossxy * 0.8 + losslen * 1.2 + lossthe * 1.5
                     
                 #iou = bbox_iou(pbox, tbox[i], CIoU=True).squeeze()  # iou(prediction, target)
