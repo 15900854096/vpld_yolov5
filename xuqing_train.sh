2b9accc88f24168e5aaaa87b6761efe36a0e854c
xuqing <qing.xu2@ofilm.com>
2025-01-06 13:20:26 +0800

    1:VOC_xuqing.yaml update train&val database

--- xuqing_train.sh
+++ xuqing_train.sh
@@ -3,7 +3,7 @@ python train.py  \
 --weights xuqing/yolov5s.pt \
 --cfg ./xuqing/yolov5s_xuqing.yaml  \
 --data ./xuqing/VOC_xuqing.yaml  \
---device 0,1,2,3 \
+--device 4,5,6,7 \
 --imgsz 640 \
 --rect \
 --epoch 900 \
--- xuqing_val.sh
+++ xuqing_val.sh
@@ -1,5 +1,5 @@
 python ./val.py \
---weights ./runs/train/20241127/weights/best.pt \
+--weights ./runs/train/20250106/weights/best.pt \
 --data ./xuqing/VOC_xuqing.yaml \
 --device 0,1,2,3 \
 --imgsz 640 \
